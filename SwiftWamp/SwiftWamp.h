338dccf323b997ae047614fad6037f88da12b270
Dany Sousa <danysousa@protonmail.com>
2016-10-13 11:15:27 +0200

    init : Create Xcode project

---
+++ SwiftWamp/SwiftWamp.h
@@ -0,0 +1,19 @@
+//
+//  SwiftWamp.h
+//  SwiftWamp
+//
+//  Created by Dany Sousa on 13/10/2016.
+//  Copyright © 2016 danysousa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftWamp.
+FOUNDATION_EXPORT double SwiftWampVersionNumber;
+
+//! Project version string for SwiftWamp.
+FOUNDATION_EXPORT const unsigned char SwiftWampVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftWamp/PublicHeader.h>
+
+
